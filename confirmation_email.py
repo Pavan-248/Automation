2d4963033166d061d857419ef6d1e789c6628d78
unknown <pavanppk448@gmail.com>
2023-10-05 10:27:37 +0530

    Modified the Python script to read email addresses from an Excel file and send confirmation emails to each recipient individually.

--- confirmation_email.py
+++ confirmation_email.py
@@ -1,11 +1,15 @@
 import smtplib
 import ssl
 from email.message import EmailMessage
+import pandas as pd
 
-# Define email sender and receiver
-email_sender = 'pavanppk448@gmail.com'
-email_password = '#password'
-email_receiver = 'pavanppk448@gmail.com'
+# Read email addresses from an Excel file
+df = pd.read_excel(r"C:\Users\pavan\OneDrive\Documents\Confirmation_list.xlsx")  # Replace with your Excel file's path
+email_addresses = df['Email'].tolist()
+
+# Define email sender
+email_sender = ''
+email_password = ''
 
 # Set the subject and body of the email
 subject = 'TEDxSIST Test Mail'
@@ -13,16 +17,21 @@ body = """
 It's Working
 """
 
-em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['Subject'] = subject
-em.set_content(body)
-
 # Add SSL (layer of security)
 context = ssl.create_default_context()
 
-# Log in and send the email
+# Log in and send separate emails to each recipient using Bcc
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+    for email_receiver in email_addresses:
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['Subject'] = subject
+        em.set_content(body)
+        em['Bcc'] = email_receiver  # Set the "Bcc" header for this specific recipient
+
+        smtp.send_message(em)
+        print(f'Email sent successfully to {email_receiver}')
+
+print('All emails sent successfully!')
