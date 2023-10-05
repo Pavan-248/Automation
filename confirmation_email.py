c00b518a685495c847cd4cc327342e5905bd45bc
unknown <pavanppk448@gmail.com>
2023-10-05 10:32:42 +0530

    Updated script with changes

--- confirmation_email.py
+++ confirmation_email.py
@@ -3,15 +3,12 @@ import ssl
 from email.message import EmailMessage
 import pandas as pd
 
-# Read email addresses from an Excel file
 df = pd.read_excel(r"C:\Users\pavan\OneDrive\Documents\Confirmation_list.xlsx")  # Replace with your Excel file's path
 email_addresses = df['Email'].tolist()
 
-# Define email sender
 email_sender = ''
 email_password = ''
 
-# Set the subject and body of the email
 subject = 'TEDxSIST Test Mail'
 body = """
 It's Working
@@ -20,7 +17,6 @@ It's Working
 # Add SSL (layer of security)
 context = ssl.create_default_context()
 
-# Log in and send separate emails to each recipient using Bcc
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
 
@@ -29,7 +25,7 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         em['From'] = email_sender
         em['Subject'] = subject
         em.set_content(body)
-        em['Bcc'] = email_receiver  # Set the "Bcc" header for this specific recipient
+        em['Bcc'] = email_receiver 
 
         smtp.send_message(em)
         print(f'Email sent successfully to {email_receiver}')
